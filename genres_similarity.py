16c62f7ae902e1ad8646b64dd28cc7f92067bccf
CuteQuokka21 <150591965+CuteQuokka21@users.noreply.github.com>
2023-11-13 23:57:01 +0300

    Update genres_similarity.py
    fix the bug (commented)

--- genres_similarity.py
+++ genres_similarity.py
@@ -7,5 +7,5 @@ with open("data/genre_similarity.json", 'r') as json_file:
 def similarityCoefficientByGenre(genre1, genre2):
     coefficient = 0
     index1 = map_ratio_genres['Genres'].index(genre1)
-    coefficient += map_ratio_genres[genre2][index1]
+    coefficient += map_ratio_genres[genre2][index1] # ----------------- ПЕРЕДЕЛАТЬ, СКЛАДЫВАТЬ НЕЛЬЗЯ ----------------
     return coefficient
