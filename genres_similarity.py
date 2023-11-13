53d23109ed0cf78c041df5ce30b8b1f7e7c10f10
CuteQuokka21 <150591965+CuteQuokka21@users.noreply.github.com>
2023-11-14 00:18:56 +0300

    Update genres_similarity.py
    new comments

--- genres_similarity.py
+++ genres_similarity.py
@@ -7,5 +7,5 @@ with open("data/genre_similarity.json", 'r') as json_file:
 def similarityCoefficientByGenre(genre1, genre2):
     coefficient = 0
     index1 = map_ratio_genres['Genres'].index(genre1)
-    coefficient += map_ratio_genres[genre2][index1] # ----------------- ПЕРЕДЕЛАТЬ, СКЛАДЫВАТЬ НЕЛЬЗЯ ----------------
+    coefficient += map_ratio_genres[genre2][index1] # ----------------- ПЕРЕДЕЛАТЬ, ПРОСТО СКЛАДЫВАТЬ НЕЛЬЗЯ (МОЖНО ПРОПУСТИТЬ ЧЕРЕЗ ФУНКЦИЮ) ----------------
     return coefficient
