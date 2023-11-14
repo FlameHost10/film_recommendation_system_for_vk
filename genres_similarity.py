0d7b66578ea1e7809543b436398f88ad43f4b2f9
Mikle <80474341+mshafiev@users.noreply.github.com>
2023-11-14 17:57:37 +0300

    Add genre filter

--- genres_similarity.py
+++ genres_similarity.py
@@ -4,8 +4,12 @@ with open("data/genre_similarity.json", 'r') as json_file:
     map_ratio_genres = json.load(json_file)
 
 
-def similarityCoefficientByGenre(our_genres, title_genres):
+
+def similarityCoefficientByGenre(our_genres):
     sum_c = 0
+    with open("data/title.txt", "r") as f:
+        title_genres = f.readline().split()
+        f.close()
     if title_genres == our_genres:
         return 0
     diff = union_minus_intersection(title_genres, our_genres)
@@ -14,17 +18,17 @@ def similarityCoefficientByGenre(our_genres, title_genres):
             cur_sum = 0
             for j in our_genres:
                 cur_sum += get_k(i, j)
-            cur_sum /= len(our_genres)
+            cur_sum = len(our_genres)
             sum_c += cur_sum
-        return sum_c / len(diff)
+        return sum_c / len(diff) * 0.8
     if len(title_genres) < len(our_genres):
         for i in diff:
-            cur_sum = 0
+            cur_sum=0
             for j in title_genres:
                 cur_sum += get_k(i, j)
             cur_sum /= len(our_genres)
             sum_c += cur_sum
-        return sum_c / len(diff)
+        return sum_c / len(diff) * 0.8
 
 
 def union_minus_intersection(list1, list2):
@@ -32,11 +36,16 @@ def union_minus_intersection(list1, list2):
     intersection_result = list(set(list1) & set(list2))
     difference = list(set(union_result) - set(intersection_result))
     return difference
-
-
 def get_k(genre1, genre2):
     coefficient = 0
     index1 = map_ratio_genres['Genres'].index(genre1)
     coefficient += map_ratio_genres[genre2][index1]
     coefficient = 1 - coefficient
+
     return coefficient
+
+
+def get_title_genres(data_title_genres):
+    with open("data/title.txt","w") as f:
+        f.write(' '.join(data_title_genres))
+        f.close()
