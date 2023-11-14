66173182e41268efd63e0e89853a6983dc53b4ea
a-tagiev <antn.tgv@gmail.com>
2023-11-15 00:10:51 +0300

    fixed some bugs and made some improvements

--- genres_similarity.py
+++ genres_similarity.py
@@ -1,47 +1,55 @@
 import json
 
+# читаем json файл с коэффициентами схожести фильмов
 with open("data/genre_similarity.json", 'r') as json_file:
     map_ratio_genres = json.load(json_file)
 
 
 def similarityCoefficientByGenre(our_genres):
-    sum_c = 0
     with open("data/title.txt", "r") as f:
         title_genres = f.readline().split()
         f.close()
-    if title_genres == our_genres:
-        return 0
     diff = union_minus_intersection(title_genres, our_genres)
+    sum_c = 0  # некоторый коэффициент схожести для жанров 2 фильмов
+
+    if len(title_genres) == 0 or len(our_genres) == 0:  # если нет жанра то по жанрам фильмы несовместимы
+        return 0
+    if title_genres == our_genres:  # если жанры одинаковы, то по жанрам фильмы идентичны
+        return 1
+
     if len(title_genres) >= len(our_genres):
         for i in diff:
             cur_sum = 0
             for j in our_genres:
-                cur_sum += get_k(i, j)
+                cur_sum += get_с(i, j)
             cur_sum = len(our_genres)
             sum_c += cur_sum
         return sum_c / len(diff) * 0.8
+
     if len(title_genres) < len(our_genres):
         for i in diff:
             cur_sum = 0
             for j in title_genres:
-                cur_sum += get_k(i, j)
+                cur_sum += get_с(i, j)
             cur_sum /= len(our_genres)
             sum_c += cur_sum
         return sum_c / len(diff) * 0.8
 
 
 def union_minus_intersection(list1, list2):
+    # хотим найти жанры, которые соответствуют только одному из двух фильмов
     union_result = list(set(list1 + list2))
     intersection_result = list(set(list1) & set(list2))
     difference = list(set(union_result) - set(intersection_result))
     return difference
 
 
-def get_k(genre1, genre2):
+def get_с(genre1, genre2):
     coefficient = 0
     index1 = map_ratio_genres['Genres'].index(genre1)
     coefficient += map_ratio_genres[genre2][index1]
-    coefficient = 1 - coefficient
+    coefficient = 1 - coefficient  # возвращаем итоговый коэффициент в main.py
+    # чем меньше коэффициент, тем более похожи жанры
     return coefficient
 
 
