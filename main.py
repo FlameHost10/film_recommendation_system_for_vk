1acf1c2a103e307091ec3705d2c236fbd97da8f3
Mikle <80474341+mshafiev@users.noreply.github.com>
2023-11-14 17:57:17 +0300

    Add genre filter

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from math import sqrt
 from datetime import datetime
-from genres_similarity import similarityCoefficientByGenre
+from genres_similarity import similarityCoefficientByGenre, get_title_genres
 
 def get_year(title):
     try:
-        return int(title[-5:-1])
+        return int(title[-5:-1]) / 10e4
     except ValueError:
         return 0
 
@@ -17,14 +17,14 @@ def get_genres(genres):
 def get_k_rating(data):
     rating = 10e10 * data['rating'] / (datetime.timestamp(datetime.now()) - data['timestamp'])
     if rating.count() < 50:
-        return rating.mean() * sqrt(rating.count())
-    return rating.median() * sqrt(rating.count())
+        return rating.mean() * sqrt(rating.count()) / 10e5
+    return rating.median() * sqrt(rating.count()) / 10e5
 
 def get_s(data):
     return sqrt(data)
 
-def get_genre_similarity(data, genres):
-    return similarityCoefficientByGenre(data, genres)
+def get_genre_similarity(data):
+    return similarityCoefficientByGenre(data)
 
 def get_similar_movies(data, title, n, genre_similarity_file='data/genres_similarity.json'):
     df = pd.DataFrame()
@@ -39,13 +39,15 @@ def get_similar_movies(data, title, n, genre_similarity_file='data/genres_simila
     year = get_year(title)
     k_rating = df['k_rating'].loc[df['title'] == title].values[0]
     title_genres = df['genres'].loc[df['title'] == title].values[0]
+    get_title_genres(title_genres)
 
 
-    df['genre_similarity'] = df['genres'].apply(get_genre_similarity, genre = title_genres) #надо добавить столбец, в котором
+    df['genre_similarity'] = df['genres'].apply(get_genre_similarity) #задаем только дату, текущий фильм отдельно надо как-то
+
     title_genre_k = df['genre_similarity'].loc[df['title'] == title].values[0]
-    print(k_rating)
-    print(year)
+
     df['s'] = (df['k_rating'] - k_rating) ** 2 + (df['year'] - year) ** 2 + (df['genre_similarity'] - title_genre_k) ** 2
+    #df['s'] = (df['k_rating'] - k_rating) ** 2 + (df['year'] - year) ** 2
     df['s'] = df['s'].apply(get_s)
 
     return df[['k_rating', 's']].sort_values(by='s').head(n + 1)
